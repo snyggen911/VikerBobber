eb240922823f91318e034d5ad79def24d523f3a8
snyggen911 <alleboii96@gmail.com>
2016-08-08 18:35:24 +0200

    Update AssemblyInfo.cs

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,25 +1,31 @@
+#region using directives
+
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+
+#endregion
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PokemonGo.RocketAPI.Logic")]
+
+[assembly: AssemblyTitle("NecroBot Logic for Pokémon GO")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PokemonGo.RocketAPI.Logic")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("PoGo.NecroBot.Logic")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
+
 [assembly: Guid("0739e40d-c589-4aeb-93e5-ee8cd6773c60")]
 
 // Version information for an assembly consists of the following four values:
@@ -32,5 +38,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.8")]
+
+[assembly: AssemblyVersion("0.7.8")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
