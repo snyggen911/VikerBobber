17ca37039f76d13a0dfb73fd0866e2aa0bb3d112
snyggen911 <alleboii96@gmail.com>
2016-08-08 15:27:59 +0200

    Update AssemblyInfo.cs

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,15 +1,15 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Pokemon Go Rocket API")]
+[assembly: AssemblyTitle("PokemonGo.RocketAPI.Logic")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Pokemon Go Rocket API")]
+[assembly: AssemblyProduct("PokemonGo.RocketAPI.Logic")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("05d2da44-1b8e-4cf7-94ed-4d52451cd095")]
+[assembly: Guid("0739e40d-c589-4aeb-93e5-ee8cd6773c60")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("10.2.8")]
+[assembly: AssemblyVersion("10.2.9")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
